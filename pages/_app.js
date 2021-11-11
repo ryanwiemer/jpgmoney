0aa836ee980af45db5bd0431ad9080b85fc0d578
ryanwiemer <ryan@ryanwiemer.com>
2021-11-10 22:05:38 -0800

    update env variable

--- pages/_app.js
+++ pages/_app.js
@@ -19,48 +19,11 @@ const App = ({ Component, pageProps, router }) => {
   useEffect(() => window.addEventListener('keydown', handleFirstTab), [])
 
   return (
-    <ThemeProvider theme={theme}>
-      <DefaultSeo
-        titleTemplate="%s — JPG Money"
-        defaultTitle="JPG Money 💰"
-        description="JPG Money is a virtual gallery of some of the most expensive and sought after NFTs."
-        canonical="https://jpgmoney.vercel.app"
-        additionalLinkTags={[
-          {
-            rel: 'icon',
-            href: '/favicon.ico',
-          },
-          {
-            rel: 'apple-touch-icon',
-            href: '/apple-touch-icon.png',
-            sizes: '180x180',
-          },
-          {
-            rel: 'manifest',
-            href: '/manifest.json',
-          },
-          {
-            name: 'theme-color',
-            content: '#f3f3f3',
-          },
-        ]}
-        openGraph={{
-          type: 'website',
-          url: 'https://jpgmoney.vercel.app',
-          site_name: 'JPGs',
-          images: [
-            {
-              url: 'https://jpgmoney.vercel.app/og.jpg',
-              width: 1200,
-              height: 630,
-            },
-          ],
-        }}
-      />
+    <>
       <Script
         strategy="lazyOnload"
         id="GA1"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
       />
       <Script id="GA2" strategy="lazyOnload">
         {`
@@ -68,26 +31,65 @@ const App = ({ Component, pageProps, router }) => {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', ${process.env.GOOGLE_ANALYTICS});
+          gtag('config', ${process.env.NEXT_PUBLIC_GA_ID});
         `}
       </Script>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      <ThemeProvider theme={theme}>
+        <DefaultSeo
+          titleTemplate="%s — JPG Money"
+          defaultTitle="JPG Money 💰"
+          description="JPG Money is a virtual gallery of some of the most expensive and sought after NFTs."
+          canonical="https://jpgmoney.vercel.app"
+          additionalLinkTags={[
+            {
+              rel: 'icon',
+              href: '/favicon.ico',
+            },
+            {
+              rel: 'apple-touch-icon',
+              href: '/apple-touch-icon.png',
+              sizes: '180x180',
+            },
+            {
+              rel: 'manifest',
+              href: '/manifest.json',
+            },
+            {
+              name: 'theme-color',
+              content: '#f3f3f3',
+            },
+          ]}
+          openGraph={{
+            type: 'website',
+            url: 'https://jpgmoney.vercel.app',
+            site_name: 'JPGs',
+            images: [
+              {
+                url: 'https://jpgmoney.vercel.app/og.jpg',
+                width: 1200,
+                height: 630,
+              },
+            ],
+          }}
         />
-        <meta name="theme-color" content="#000000"></meta>
-      </Head>
-      <Global styles={global} />
-      <Menu />
-      <AnimatePresence
-        exitBeforeEnter
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
-      <Background />
-    </ThemeProvider>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+          <meta name="theme-color" content="#000000"></meta>
+        </Head>
+        <Global styles={global} />
+        <Menu />
+        <AnimatePresence
+          exitBeforeEnter
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+        <Background />
+      </ThemeProvider>
+    </>
   )
 }
 
